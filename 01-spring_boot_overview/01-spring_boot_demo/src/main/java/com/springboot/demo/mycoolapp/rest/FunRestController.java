afabc394b44c3ba8bbc57c7eef4ccbc4fccc557a
ti lee <tileeling1110@gmail.com>
2025-02-23 13:33:44 +0800

    update to latest

---
+++ 01-spring_boot_overview/01-spring_boot_demo/src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,15 @@
+package com.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World"
+
+    @GetMapping("/")    // tells Spring Boot what URL this method should respond to (example: http://localhost:8080/)
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
