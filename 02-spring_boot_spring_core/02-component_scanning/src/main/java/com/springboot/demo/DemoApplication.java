36e91c67805aced0f096cce4ca4d1de1b6493d1c
LING TI LEE <134474073+tilee00@users.noreply.github.com>
2025-03-30 07:11:57 +0000

    update package scanner

--- 02-spring_boot_spring_core/02-component_scanning/src/main/java/com/springboot/demo/DemoApplication.java
+++ 02-spring_boot_spring_core/02-component_scanning/src/main/java/com/springboot/demo/DemoApplication.java
@@ -3,6 +3,12 @@ package com.springboot.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @SpringBootApplication(
+// 	scanBasePackages = {
+// 		"com.springboot.demo",
+// 		"com.springboot.util"
+// 	}
+// )
 @SpringBootApplication
 public class DemoApplication {
 
